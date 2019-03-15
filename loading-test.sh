dbee61736e922abe29f3771c976f50ff7dd5df11
Sing.Z <esin.zhong@gmail.com>
2019-03-15 09:28:48 +0800

    loading效果

---
+++ loading-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 测试loading效果
+./loading3.sh load
+
+sleep 5 
+./loading3.sh ok
---
+++ loading.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+loading(){
+    echo "loading">/tmp/load.tmp
+    i=0
+    index=0
+    arr=( "|" "/" "-" "\\" )
+    while [ $i -ge 0 ]
+    do
+        let index=index%4
+        printf "...[%s]\r" "${arr[$index]}"
+        let i++
+        let index++
+        sleep 0.05
+        load=`cat /tmp/load.tmp`
+        if [ "done" == "$load" ];then
+            break
+        fi
+    done
+    printf "...[ok]\n"
+}
+
+load_done(){
+    echo "done">/tmp/load.tmp
+}
+
+case "$1" in
+   load) loading &;;
+   ok) load_done;;
+   *) ;;
+esac
